1b0565055008952f8f93ec93ba2bb66f2007e37f
ShubhamL025 <swarashubham20@gmail.com>
2025-06-08 00:00:31 -0500

    Saturday( 3rd, 6/7 )
    Added one condition to try keeping car in the center by difference of left and right sensor. Also adjusting some velocity with some changes in threshold.

--- MyAgent.py
+++ MyAgent.py
@@ -12,9 +12,13 @@ class Agent:
         right = sensor[4]
 
 # Controlling the Steering of the car to turn left or right.
-        if right<1.24 or midright<1.43:
+        if right<1.24 or midright<1.45:
            steering='left'
-        elif left<1.24 or midleft<1.43:
+        elif left<1.24 or midleft<1.45:
+            steering = 'right'
+        elif left-right>1:
+            steering = 'left'
+        elif right-left>1:
             steering = 'right'
         else:
             steering = 'straight'
@@ -22,8 +26,8 @@ class Agent:
 # Go ahead or brake controls.
         if front > 1.3 and velo_car < 0.25:
             goahead = 'accelerate'
-        elif front < 0.5 or velo_car > 0.8:
-            goahead = 'brake'
+        elif front <0.6 or velo_car>0.4:
+            goahead = 'coast'
         else:
             goahead = 'coast'
 
